727092306021da1161663dd5dec65fbff1dc2585
klpkt <metareibo@gmail.com>
2026-01-06 19:31:56 +0100

    TextNode to HTMLNode conversion

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -12,6 +12,23 @@ class TestTextNode(unittest.TestCase):
         self.assertNotEqual(node1, node3)
         node4 = TextNode("This is a link", TextType.LINK, "google.com")
         self.assertNotEqual(node3, node4)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+    
+    def test_bold(self):
+        node = TextNode("this is bolded", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, 'b')
+    
+    def test_link(self):
+        node = TextNode("this is a link", TextType.LINK, "en.wikipedia.org")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, 'a')
+        self.assertEqual(html_node.props['href'], "en.wikipedia.org")
 
 if __name__ == "__main__":
     unittest.main()
