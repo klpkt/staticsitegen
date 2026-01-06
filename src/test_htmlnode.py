474f19f670b050516319f4365ee8937664e3effa
klpkt <metareibo@gmail.com>
2026-01-06 19:09:48 +0100

    add leaf nodes

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,12 +1,12 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_init(self):
-        node = HTMLNode('p', "Hello world")
+        node = HTMLNode('p', "Hello, world!")
         self.assertEqual(node.tag, 'p')
-        self.assertEqual(node.value, "Hello world")
+        self.assertEqual(node.value, "Hello, world!")
         self.assertIsNone(node.children)
         self.assertIsNone(node.props)
 
@@ -14,18 +14,30 @@ class TestHTMLNode(unittest.TestCase):
         self.assertEqual(node2.props["href"], "en.wikipedia.org")
 
     def test_repr(self):
-        node = HTMLNode('p', "Hello world")
-        self.assertEqual(repr(node), "HTMLNode(p, Hello world, None, None)")
+        node = HTMLNode('p', "Hello, world!")
+        self.assertEqual(repr(node), "HTMLNode(p, Hello, world!, None, None)")
 
         node2 = HTMLNode('a', "Wikipedia", None, {"href": "en.wikipedia.org"})
         self.assertEqual(repr(node2), "HTMLNode(a, Wikipedia, None, {'href': 'en.wikipedia.org'})")
     
     def test_props(self):
-        node = HTMLNode('p', "Hello world")
+        node = HTMLNode('p', "Hello, world!")
         self.assertEqual(node.props_to_html(), "")
         
         node2 = HTMLNode('a', "Wikipedia", None, {"href": "en.wikipedia.org"})
         self.assertEqual(node2.props_to_html(), " href=\"en.wikipedia.org\"")
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode('p', "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+    
+    def test_leaf_to_html_a(self):
+        node = LeafNode('a', "Wikipedia", {"href": "en.wikipedia.org"})
+        self.assertEqual(node.to_html(), '<a href="en.wikipedia.org">Wikipedia</a>')
+
+    def test_leaf_repr(self):
+        node = LeafNode('p', "Hello, world!")
+        self.assertEqual(repr(node), "LeafNode(p, Hello, world!, None)")
 
 if __name__ == "__main__":
     unittest.main()
